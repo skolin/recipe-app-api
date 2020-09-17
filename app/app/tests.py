d511226b083d027fad086fc3b87a60bff5351df8
skolin <sri@skware.us>
2020-09-17 19:21:53 -0400

    Removed adding calc (not needed)

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.count import add
 
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are correctly added"""
-        self.assertEqual(add(3,8),11)        
+        self.assertEqual(add(3,8),11)
